1ad96f615fa6c1b198d0344e3f8c2b504826f404
anwar108658 <anwar108658@gmail.com>
2025-01-31 06:38:43 +0500

    label name

--- src/page/Update.jsx
+++ src/page/Update.jsx
@@ -57,7 +57,7 @@ const Update = () => {
     </div>
     <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
       <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-first-age">
-        Email
+      Age
       </label>
       <input className="appearance-none block w-full bg-gray-200 text-gray-700  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-age" type="number" value={dataVal?.age} name="age" onChange={(e) => changeHandler(e)} placeholder="Age" />
     </div>
